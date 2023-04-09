a76836fb09e7904b79da51825ad28069b815616d
Yao <meizhen.yao@mssm.edu>
2023-04-09 13:30:28 -0400

    descriptive analysis

---
+++ BioMe_proteome_descriptive.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(ggpubr)
+library(data.table)
+
+
+# import data (fread)
+BioMe_proteome <- read.table("C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/BioMe/proteome/input/Q-00771_Loos_NPX_2022-05-03/Q-00771_Loos_NPX_2022-05-03.txt", header = TRUE)
